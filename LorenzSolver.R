f28d77c98151b8e672df96e569ff9a7c341e33fb
dcarvajalp <dcarvajalp@gmail.com>
2014-11-14 23:39:23 +0100

    Modifié fichier LorenzSolver.R pour resoudre le ODE

--- LorenzSolver.R
+++ LorenzSolver.R
@@ -8,3 +8,31 @@ source("clear.R")
 library(deSolve)
 source("Lorenz.R")
 clc()
+
+
+# Model Parameters
+parameters <- c(a = -8/3,
+                b = -10,
+                c = 28)
+
+# State Variables
+state <- c(X = 1,
+           Y = 1,
+           Z = 1)
+
+# Time Specification
+times <- seq (0, 100, by = 0.01)
+
+
+# Model Integration
+
+out <- ode(y = state, times = times, func = Lorenz, parms = parameters)
+head(out)
+
+#Plotting Results
+png(filename = "Lorenz.png", width = 480, height = 480, units = "px")
+par(oma = c(0, 0, 3, 0))
+plot(out, xlab = "time", ylab = "-")
+plot(out[, "X"], out[, "Z"], pch = ".")
+mtext(outer = TRUE, side = 3, "Lorenz model", cex = 1.5)
+dev.off()
